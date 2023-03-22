535ed3dcc2891b9248c5d1e2ac5b192779899fb7
Juan Buitrago <buitragomjuan@outlook.com>
2023-03-21 19:36:53 -0500

    se agregan los archivos

---
+++ BinaryNode.h
@@ -0,0 +1,18 @@
+#ifndef __BINARYNODE_H__
+#define __BINARYNODE_H__
+
+
+template <class T>
+class BinaryNode {
+
+    protected:
+
+
+    public:
+
+
+};
+
+
+#include "BinaryNode.cxx"
+#endif
---
+++ BinaryTree.h
@@ -0,0 +1,21 @@
+#ifndef __BINARYTREE_H__
+#define __BINARYTREE_H__
+
+#include "BinaryNode.h"
+
+template <class T>
+class BinaryTree {
+
+    protected:
+
+
+
+    public: 
+
+
+
+
+};
+
+#include "BinaryTree.cxx"
+#endif
